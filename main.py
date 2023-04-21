0f43e646235ea3bc09d18bea2ed4d33cd05f6f6e
Krest01 <jakubnagadowski123@gmail.com>
2023-04-21 02:54:17 +0200

    almost done project

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ cross_probability = 0.8
 mutation_probability = 0.1
 max_weight = 89
 population_size = 20
+generations = 50
 
 
 def read_file(file):
@@ -32,17 +33,17 @@ def create_solution(items):
     return solution
 
 
-# def check_solution(items, solution, max_weight):
-#     total_weight = 0
-#     for i in solution:
-#         if i == 1:
-#             total_weight = total_weight + items[solution.index(i)].weight
-#         else:
-#             pass
-#     if total_weight > max_weight:
-#         return check_solution(items, create_solution(items), max_weight)
-#     else:
-#         return True
+def check_solution(items, solution, max_weight):
+    total_weight = 0
+    for i in solution:
+        if i == 1:
+            total_weight = total_weight + items[solution.index(i)].weight
+        else:
+            pass
+    if total_weight > max_weight:
+        return False
+    else:
+        return True
 
 
 def check_value(items, solution):
@@ -53,64 +54,57 @@ def check_value(items, solution):
     return value
 
 
-def check_duplicates(solution1, solution2):
-    if solution1 == solution2:
-        return True
-    else:
-        return False
+# def check_duplicates(solution_1, solution_2):
+#     if solution_1 == solution_2:
+#         return True
+#     else:
+#         return False
 
 
-def initialize_population(population_size, items, max_weight):
+def initialize_population(population_size, items):
     population = []
     while len(population) < population_size:
-        print(population)
         new_solution = create_solution(items)
-        print(new_solution)
-        if len(population) > 1:
-            for i in population:
-                if check_duplicates(new_solution, i):
-                    pass
-                else:
-                    population.append(new_solution)
-                # else:
-                #     if check_solution(items, new_solution, max_weight):
-                #         population.append(new_solution)
-                #     else:
-                #         pass
-        else:
-            population.append(new_solution)
+        population.append(new_solution)
     return population
 
 
 def tournament_selection(population):
     first_individual = population[random.randint(0, len(population) - 1)]
     second_individual = population[random.randint(0, len(population) - 1)]
-    if check_value(items,first_individual) > check_value(items, second_individual):
+    if check_value(items, first_individual) > check_value(items, second_individual):
         survive = first_individual
     else:
         survive = second_individual
     return survive
 
 
-def single_point_crossover(parent1, parent2):
-    new_parent1 = np.append(parent1[:3], parent2[3:])
-    new_parent2 = np.append(parent2[:3], parent1[3:])
-    new_parent1.tolist()
-    new_parent2.tolist()
-    return new_parent1, new_parent2
+def single_point_crossover(parent_1, parent_2):
+    point = np.random.randint(0, len(parent_1) - 1)
+    child_1 = parent_1[:point] + parent_2[point:]
+    child_2 = parent_2[:point] + parent_1[point:]
+    return child_1, child_2
 
 
 def inversion_mutation(chromosome):
-    print(chromosome)
-    start = random.randint(0, round(len(chromosome)/2))
-    stop = random.randint(start, len(chromosome))
-    mutation = chromosome[start:stop].reverse()
-    print(mutation)
-    return mutation
+    for i in range(len(chromosome)):
+        if chromosome[i] == 0:
+            chromosome[i] = 1
+        else:
+            chromosome[i] = 0
+    return chromosome
+
+
+# def genetic_algorithm(max_weight, cross_probability, mutation_probability, file, population__size, generations):
+#     population = initialize_population()
+#     for generation in range(generations - 1):
 
 
 if __name__ == "__main__":
     items = read_file(file)
     population = initialize_population(population_size, items, max_weight)
     chromosome = random.choice(population)
+    parent1 = random.choice(population)
+    parent2 = random.choice(population)
+    single_point_crossover(parent1, parent2)
     inversion_mutation(chromosome)
